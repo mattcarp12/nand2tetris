75491f2a45817fa3b1896c1f0a1d3e14b68eab27
mcarpenter <mattcarp88@gmail.com>
2019-07-21 11:05:29 -0400

    complete project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,34 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@R0
+M=0
+
+(KEYMONITOR)
+	@KBD
+	D=M
+	@FILL
+	D;JNE
+	@KEYMONITOR
+	0;JMP
+(FILL)
+	@R0		
+	D=M		// D = R0
+	@4096
+	D=D-A	// D = R0 - 1
+	@END
+	D;JGE	// If (R0 - 4096) > 0 goto end
+	@R0 	// load current screen offset
+	D=M		// store offset to D 
+	@SCREEN
+	A=A+D	// set A to currenet pixel
+	M=0  	
+	M=!M 	// turn pixel on
+	@R0		// increment offset
+	M=M+1
+	@FILL
+	0;JMP
+(END)
+	@END
+	0;JMP
